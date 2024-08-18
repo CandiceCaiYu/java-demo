84a731fddc7edf2f4eb7edeb56f78381371e2e97
Candice <6caiyu6@gmail.com>
2024-08-18 11:10:51 +0800

    fix: add setter and getter in entity to get data from repository

--- src/main/java/com/example/demo/dao/Student.java
+++ src/main/java/com/example/demo/dao/Student.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
-@Table(name = "Student")
+@Table(name = "student")
 public class Student {
     @Id
     @Column(name = "id")
@@ -20,4 +20,36 @@ public class Student {
 
     @Column(name = "age")
     private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
